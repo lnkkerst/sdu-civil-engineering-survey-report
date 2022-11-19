78a713f897dd4e37a5d5466b4f99659d684c5c91
lnkkerst <lnkkerst04@gmail.com>
2022-11-20 01:14:22 +0800

    fix: vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
@@ -12,7 +12,7 @@ import '@mdi/font/css/materialdesignicons.css';
 
 const app = createApp(App);
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 });
 app.use(router);
